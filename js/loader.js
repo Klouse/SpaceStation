6779f96170c5be66e493dd01addf7082ee33b8ed
Matt Zeosky <mjz4277@gmail.com>
2014-04-30 14:37:43 -0400

    Basic structure from city ICE

---
+++ js/loader.js
@@ -0,0 +1,87 @@
+/*
+loader.js
+variable app is in global scope - i.e. a property of window.
+app is our single global object literal - all other functions and properties of 
+the bubbles game will be properties of app.
+*/
+"use strict";
+
+// if app exists use the existing copy
+// else create a new object literal
+var app = app || {};
+
+// CONSTANTS of app
+app.KEYBOARD = {
+	"KEY_LEFT": 37, 
+	"KEY_UP": 38, 
+	"KEY_RIGHT": 39, 
+	"KEY_DOWN": 40,
+	"KEY_SPACE": 32
+};
+
+// properties of app
+app.animationID = undefined;
+app.paused = false;
+
+// key daemon array
+app.keydown = [];
+
+(function(){
+	var queue = new createjs.LoadQueue(false);
+	queue.on("fileload", handleFileLoad, this);
+	queue.on("complete", complete, this);
+	queue.loadFile("js/lib/three.min.js");
+	queue.loadFile("js/lib/FirstPersonControls.js");
+	queue.loadFile("js/main.js");
+	
+	function handleFileLoad(e){
+		console.log(e + " loaded");
+	}
+	
+	function handleComplete(e){
+		app.main.init();
+	}
+	
+	// when the loading is complete, this function will be called
+		 function complete(){
+			
+			// set up event handlers
+			window.onblur = function(){
+				app.paused = true;
+				cancelAnimationFrame(app.animationID);
+				app.keydown = []; // clear key daemon
+				// call update() so that our paused screen gets drawn
+				app.main.update();
+			};
+			
+			window.onfocus = function(){
+				app.paused = false;
+				cancelAnimationFrame(app.animationID);
+				// start the animation back up
+				app.main.update();
+			};
+			
+			// event listeners
+			window.addEventListener("keydown",function(e){
+				console.log("keydown=" + e.keyCode);
+				app.keydown[e.keyCode] = true;
+			});
+				
+			window.addEventListener("keyup",function(e){
+				console.log("keyup=" + e.keyCode);
+				app.keydown[e.keyCode] = false;
+			});
+			
+			
+			// start game
+			app.main.init();
+		} // end complete
+
+}());
+
+
+
+		
+		
+		
+
---
+++ js/main.js
@@ -0,0 +1,127 @@
+// city.js
+"use strict";
+// if app exists use the existing copy
+// else create a new object literal
+var app = app || {};
+
+app.main = {
+    	// CONSTANT properties
+    	
+		// variable properties
+		renderer: undefined,
+		scene: undefined,
+		camera: undefined,
+		myobjects: [],
+		paused: false,
+		dt: 1/60,
+		controls: undefined,
+		
+		
+    	init : function() {
+			console.log('init called');
+			this.setupThreeJS();
+			this.setupWorld();
+			this.update();
+    	},
+    	
+    	
+    update: function(){
+    	// schedule next animation frame
+    	app.animationID = requestAnimationFrame(this.update.bind(this));
+    	
+		// PAUSED?
+		if (app.paused){
+			this.drawPauseScreen();
+			return;
+		 }
+	
+		// UPDATE
+		this.controls.update(this.dt);
+		
+		// DRAW	
+		this.renderer.render(this.scene, this.camera);
+		
+	},
+	
+	setupThreeJS: function() {
+				this.scene = new THREE.Scene();
+				this.scene.fog = new THREE.FogExp2(0x9db3b5, 0.002);
+
+				this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+				this.camera.position.y = 400;
+				this.camera.position.z = 400;
+				this.camera.rotation.x = -45 * Math.PI / 180;
+
+				this.renderer = new THREE.WebGLRenderer({antialias: true});
+				this.renderer.setSize( window.innerWidth, window.innerHeight );
+				this.renderer.shadowMapEnabled = true;
+				document.body.appendChild(this.renderer.domElement );
+
+				this.controls = new THREE.FirstPersonControls(this.camera);
+				this.controls.movementSpeed = 100;
+				this.controls.lookSpeed = 0.1;
+				this.controls.autoForward = false;
+			},
+			
+	setupWorld: function() {
+				var geo = new THREE.PlaneGeometry(2000, 2000, 40, 40);
+				var mat = new THREE.MeshPhongMaterial({color: 0x9db3b5, overdraw: true});
+				var floor = new THREE.Mesh(geo, mat);
+				floor.rotation.x = -0.5 * Math.PI;
+				floor.receiveShadow = true;
+				this.scene.add(floor);
+			
+				// make a base cube geometry for all of the buildings
+				var geometry = new THREE.CubeGeometry( 1, 1, 1 );
+				// move pivot point to bottom of cube instead of center
+				geometry.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0.5, 0 ) );
+			
+
+				
+				var cityGeometry = new THREE.Geometry();
+				for (var i = 0; i < 300; i++) {
+					var building = new THREE.Mesh(geometry.clone());
+					building.position.x = Math.floor( Math.random() * 200 - 100 ) * 4;
+					building.position.z = Math.floor( Math.random() * 200 - 100 ) * 4;
+					building.scale.x  = Math.pow(Math.random(), 2) * 50 + 10;
+					building.scale.y  = Math.pow(Math.random(), 3) * building.scale.x * 8 + 8;
+					building.scale.z  = building.scale.x;
+					// merge!
+					// we have a single geometry, so it renders faster
+					THREE.GeometryUtils.merge(cityGeometry, building);
+				}
+				
+				
+				var material = new THREE.MeshPhongMaterial({color: 0xffcccc});
+				// uncomment these for a transparent city
+				//material.transparent = true;
+                //material.opacity = 0.8;
+                
+				var city = new THREE.Mesh(cityGeometry, material);
+				city.castShadow = true;
+				city.receiveShadow = true;
+				this.scene.add(city);
+
+				// the "sun"
+				var light = new THREE.DirectionalLight(0xf9f1c2, 1);
+				light.position.set(500, 1500, 1000);
+				light.castShadow = true;
+				light.shadowMapWidth = 2048;
+				light.shadowMapHeight = 2048;
+				var d = 1000;
+				// "near" and "far" of shadows and camera
+				light.shadowCameraLeft = d;
+				light.shadowCameraRight = -d;
+				light.shadowCameraTop = d;
+				light.shadowCameraBottom = -d;
+				light.shadowCameraFar = 2500;
+				this.scene.add(light);
+			},
+
+	
+	drawPauseScreen: function(){
+		// do something pause-like if you want
+	}
+	
+	
+};
